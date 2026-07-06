fa4e3038173f714786c6970ea17377fb4351c307
Elaina-Vogel <evogel@pcsknox.com>
2026-07-06 16:24:45 -0400

    Add Screen and Exit event

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # initilize game
+    pygame.init()
+
+    # set new instance of GUI Window
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # infinite loop for game loop
+    while True:
+        log_state()
+
+        # start processing the pygame event queue
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
